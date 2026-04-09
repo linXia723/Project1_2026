fffb815fe908bb72f1e6dee00f6b0747f74bcc0e
XiaLin <18014911082@163.com>
2026-04-09 21:43:50 +0800

    The button click is detected.

--- reaction.py
+++ reaction.py
@@ -1,8 +1,22 @@
 from gpiozero import LED,Button
 from time import sleep
 from random import uniform
+
 led=LED(4)
+right_button=Button(15)
+left_button=Button(14)
+
+
+def pressed(button):
+	print(str(button.pin.number)+'won the game')
+
+right_button.when_pressed=pressed
+left_button.when_pressed=pressed
+
 
 led.on()
 sleep(uniform(5,10))
 led.off()
+
+while True:
+	sleep(1)
