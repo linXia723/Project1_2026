845c79b97311c01c4c2db15e4a44095984cdc007
XiaLin <18014911082@163.com>
2026-04-10 14:31:30 +0800

    Add a score function to count scores.

--- reaction.py
+++ reaction.py
@@ -2,6 +2,9 @@ from gpiozero import LED,Button
 from time import sleep
 from random import uniform
 
+left_score=0
+right_score=0
+
 led=LED(4)
 right_button=Button(15)
 left_button=Button(14)
@@ -10,13 +13,17 @@ left_name=input('left player name is ')
 right_name=input('right player name is ')
 game_over = False
 def pressed(button):
-	global game_over
+	global game_over,left_score,right_score
 	if game_over:
 		return
 	if button.pin.number==14:
-        	print(left_name+' won the game')
+		left_score+=1
+		print(left_name+' won the game')
+		print(f"{left_name}:{right_name}={left_score}:{right_score}")
 	else:
-                print(right_name+' won the game')
+		right_score+=1
+		print(right_name+' won the game')
+		print(f"{left_name}:{right_name}={left_score}={left_score}:{right_score}")
 	game_over = True
 
 right_button.when_pressed=pressed
